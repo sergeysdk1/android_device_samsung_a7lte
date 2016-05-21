ccf145cbc588a0117e5b242e146e3bd10e4ef237
Jackeagle <jackeagle102@gmail.com>
2016-05-21 19:23:40 +0530

    a7lte: Add NFC Enhanced
    Change-Id: I7da2fda4dff342a4a3f06bcb00c7d7f76c9e9a83
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- cm.mk
+++ cm.mk
@@ -21,6 +21,9 @@ $(call inherit-product, device/samsung/a7lte/device.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a7lte
 PRODUCT_NAME := cm_a7lte
