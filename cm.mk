36b6bc8b01681b851900bac68b865086d38ca15f
sergeysdk1 <sdk@fromru.com>
2017-05-21 08:00:08 +0300

    Build for cm-14.1
    Change-Id: I585dfad3afd3ca733df4de4c806abc068d13a2c8

--- cm.mk
+++ cm.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/samsung/a7lte/device.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a7lte
